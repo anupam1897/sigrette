fefc4ee9325adb917034363389d26f37825bf705
anupam1897 <anupam1897@gmail.com>
2024-05-12 21:12:53 +0530

    Changed whole App. Initiating new React App. Version 1.0.0

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="centered-div">
+        <h1>Hello, World!</h1>
+        <p>This div is centered both horizontally and vertically on the page.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
